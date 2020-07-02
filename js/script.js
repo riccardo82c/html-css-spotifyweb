39bcd4ae71033dc377a1049aebd540012fe10957
Riccardo Colombo <rikyregna@gmail.com>
2020-07-02 12:30:22 +0200

    burge onclick effect

--- js/script.js
+++ js/script.js
@@ -9,10 +9,16 @@ function toggleTheme() {
 
 // Get the button, and when the user clicks on it, execute myFunction
 document.getElementById("myBtn").onclick = function() {
-    myFunction()
+    myFunction(),
+    burger(this)
 };
 
 /* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// burger toggle between adding and removing the class .change from the 3 bar inside burger button
+function burger(x) {
+  x.classList.toggle("change");
 }
