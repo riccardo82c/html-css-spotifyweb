c87a11b6d6cbdda6fc9332f2caf0c9819cee1823
Riccardo Colombo <rikyregna@gmail.com>
2020-07-02 11:39:06 +0200

    dropdown menu

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 function toggleTheme() {
- // If document have an attribute data-theme with value dark, set attribute to light, else set to dark.
- if (document.documentElement.getAttribute('data-theme') === 'dark')
-  document.documentElement.setAttribute('data-theme', 'light')
- else
-  document.documentElement.setAttribute('data-theme', 'dark')
+    // If document have an attribute data-theme with value dark, set attribute to light, else set to dark.
+    if (document.documentElement.getAttribute('data-theme') === 'dark')
+        document.documentElement.setAttribute('data-theme', 'light')
+    else
+        document.documentElement.setAttribute('data-theme', 'dark')
+}
+
+
+// Get the button, and when the user clicks on it, execute myFunction
+document.getElementById("myBtn").onclick = function() {
+    myFunction()
+};
+
+/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
 }
